409593b11824f40d71ef0990f127dbb2ebf198e1
Benjlida <benjlida-1986@hotmail.fr>
2026-08-27 23:44:51 +0200

    Ajouter le service worker UAG

---
+++ uag/service-worker.js
@@ -0,0 +1,68 @@
+const CACHE = 'lm-pilot-pro-uag-v1';
+const CACHE_PREFIX = 'lm-pilot-pro-uag-';
+
+const SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './offline.html',
+  './icon-uag.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys
+          .filter(key => key.startsWith(CACHE_PREFIX) && key !== CACHE)
+          .map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const request = event.request;
+  if (request.method !== 'GET') return;
+
+  const url = new URL(request.url);
+
+  if (request.mode === 'navigate' && url.origin === self.location.origin) {
+    event.respondWith(
+      fetch(request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put(request, copy));
+          return response;
+        })
+        .catch(() => caches.match(request).then(response =>
+          response || caches.match('./offline.html')
+        ))
+    );
+    return;
+  }
+
+  if (url.origin === self.location.origin) {
+    event.respondWith(
+      caches.match(request).then(cached => {
+        const network = fetch(request)
+          .then(response => {
+            if (response && response.status === 200 && response.type === 'basic') {
+              const copy = response.clone();
+              caches.open(CACHE).then(cache => cache.put(request, copy));
+            }
+            return response;
+          })
+          .catch(() => cached);
+        return cached || network;
+      })
+    );
+  }
+});
